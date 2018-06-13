e01a53af9188a3d1261355e30dc2d3b5beb9602c
izznogooood <ama@getmail.no>
2018-06-13 12:38:46 +0200

    copied from github

---
+++ README.md
@@ -0,0 +1,39 @@
+  
+# ftpscan
+
+Scan provided subnet for ftp-servers with provided username / password.
+
+## Getting Started
+
+```
+git clone https://github.com/izznogooood/ftpscan.git
+cd ftpscan
+python ftpscan.py <cidr> <username> <password>
+```
+
+What python command you use varies on different operating systems.
+
+### Help Content:
+
+```
+positional arguments:
+  cidr                  Subnet to scan <192.168.0.0/24>
+  user                  <username> to attempt login.
+  password              <password> to attempt login.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  Filename to store results.
+```
+
+### Prerequisites
+
+What do I need to run this thing?
+
+```
+Python 3,5
+```
+
+### What is CIDR?
+
+https://mxtoolbox.com/subnetcalculator.aspx
---
+++ ftpscan.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+import ftplib
+import ipaddress
+import socket
+import argparse
+import os
+
+
+def main():
+    """ Main Program """
+
+    # todo: add multithreading
+
+    result = scan(args.cidr, args.user, args.password)
+    if args.file:
+        _write_results(result, os.path.join(os.getcwd(), args.file))
+
+
+def scan(cidr, user, password):
+    """
+    Scan provided subnet for ftp access with provided user/password.
+    :param cidr: Subnet, <192.168.0.0/24>
+    :param user: Username to attempt login.
+    :param password: Password to attempt login
+    :return: IP list of successful login's.
+    """
+    network = ipaddress.ip_network(cidr)
+    result = []
+
+    for ip in network.hosts():
+        try:
+            ftp = ftplib.FTP(host=str(ip), timeout=2)
+            feedback = ftp.login(user, password)
+            ftp.quit()
+            if feedback == '230 User logged in, proceed.':
+                result.append(str(ip))
+                print('{} Successful login.'.format(ip))
+
+        except KeyboardInterrupt:
+            exit()
+        except ConnectionRefusedError:
+            print('{} Connection refused.'.format(ip))
+        except ftplib.error_perm:
+            print('{} Wrong user/password.'.format(ip))
+        except socket.timeout:
+            print("{} Timeout.".format(ip))
+        except:
+            print("{} Unknown error.".format(ip))
+
+    return result
+
+
+def _write_results(result, filename):
+    """
+    Writes a list / tuple item by item to a file.
+    :param result: IP list of successful login's.
+    :param filename: Filename to write results.
+    """
+    print("Writing list of successful login's to: {}".format(filename))
+    with open(filename, 'w') as f:
+        for ip in result:
+            f.writelines(ip + '\n')
+
+
+if __name__ == '__main__':
+
+    # Initialize CLI arguments
+
+    parser = argparse.ArgumentParser(
+        description='Scans for ftp-servers with provided user / password'
+    )
+    parser.add_argument('cidr', help='Subnet to scan <192.168.0.0/24>')
+    parser.add_argument('user', help='<username> to attempt login.')
+    parser.add_argument('password', help='<password> to attempt login.')
+    parser.add_argument('-f', '--file', help='Filename to store results.')
+    args = parser.parse_args()
+    main()
